35bc3d8d1a5e998be33825af5f83ff705998f257
munazir21 <92munazir@gmail.com>
2024-07-24 08:16:54 +0530

    MaterialTypeMaster

---
+++ materialTypeMaster/src/main/java/in/co/greenwave/materialTypeMaster/MaterialTypeMasterApplication.java
@@ -0,0 +1,13 @@
+package in.co.greenwave.materialTypeMaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaterialTypeMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaterialTypeMasterApplication.class, args);
+	}
+
+}
---
+++ materialTypeMaster/src/test/java/in/co/greenwave/materialTypeMaster/MaterialTypeMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package in.co.greenwave.materialTypeMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaterialTypeMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
