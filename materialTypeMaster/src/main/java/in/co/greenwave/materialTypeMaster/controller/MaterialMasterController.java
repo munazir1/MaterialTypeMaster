9c1105d52442959467da8d23db0387481b22d442
munazir21 <92munazir@gmail.com>
2024-08-06 15:24:16 +0530

    in getDescription

--- materialTypeMaster/src/main/java/in/co/greenwave/materialTypeMaster/controller/MaterialMasterController.java
+++ materialTypeMaster/src/main/java/in/co/greenwave/materialTypeMaster/controller/MaterialMasterController.java
@@ -91,7 +91,7 @@ public class MaterialMasterController {
 				
 				  materialMaster.getDescription(); materialMaster.getInternalMaterialCode();
 				  
-				  InternalMaterialCode.put("sapCode", materialMaster.getSapCode());
+				  InternalMaterialCode.put("internalMaterialCode", materialMaster.getSapCode());
 				  InternalMaterialCode.put("description", materialMaster.getDescription());
 			 
 			 
